bb48b10ecb16f81458ca16f2fe62bf991dfcb867
Josue <josu49067@gmail.com>
2024-08-15 14:45:02 -0600

    Cambio env

--- app/config/env.js
+++ app/config/env.js
@@ -4,8 +4,8 @@ const env = {
   database: 'umg_ejercicio_2403',
   username: 'elmerousuario',
   password: 'AgPH2Qi84ItuTiWlyLezy3dhkOwLEisg',
-  host: 'dpg-cqc7lpeehbks738amlkg-a.oregon-postgres.render.com',
-  //host: 'dpg-cqc7lpeehbks738amlkg-a',
+  //host: 'dpg-cqc7lpeehbks738amlkg-a.oregon-postgres.render.com',
+  host: 'dpg-cqc7lpeehbks738amlkg-a',
   dialect: 'postgres',
   pool: {
     max: 5,
